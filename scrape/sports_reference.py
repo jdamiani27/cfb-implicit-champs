e6f9ed3d9677af5a2299b619162f64472c5bfa56
Jason Damiani <me@jasondamiani.com>
2018-12-02 16:08:41 -0500

    first commit

---
+++ scrape/sports_reference.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+
+url = 'https://www.sports-reference.com/cfb/years/2017-schedule.html'
+
+r = requests.get(url)
+
+soup = BeautifulSoup(r.text)
+table = soup.find('table', {'id': 'schedule'})
+tbody = table.find('tbody')
+
+
+def not_of_class_thead(class_):
+    return 'thead' != class_
+
+
+data_rows = tbody.find_all('tr', class_=not_of_class_thead)
+
+include_stats = ['week_number', 'date_game', 'winner_school_name',
+                 'winner_points', 'game_location', 'loser_school_name',
+                 'loser_points', 'notes']
+data = [[''.join(col.findAll(text=True))
+         for col in row.find_all('td', {'data-stat': include_stats})]
+        for row in data_rows]
+
+df = pd.DataFrame(data, columns=include_stats)
+df['date_game'] = pd.to_datetime(df['date_game'], infer_datetime_format=True)
+
+for col in ['week_number', 'winner_points', 'loser_points']:
+    df[col] = df[col].astype('int')
+
+for team in ['winner', 'loser']:
+    df[[f'{team}_rank', f'{team}_school_name']] = df[f'{team}_school_name'].str.extract(r'(?:\((\d+)\)\s)?(.*)')
+df.head()
